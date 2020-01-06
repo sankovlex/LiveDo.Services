75dd8f12e5c76f41c92cdd4735753bfc72d05707
Alex Sankov <sankovlex@gmail.com>
2020-01-06 19:38:39 +0400

    Remove unused projects

--- src/auth/Domain/LiveDo.Auth.Domain.Users.Services/IPasswordHasher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LiveDo.Auth.Domain.Users.Services
-{
-	public interface IPasswordHasher
-	{
-		bool VerifyHashedPassword(string hashedPassword, string providedPassword);
-		
-		string HashPassword(string password);
-	}
-}
